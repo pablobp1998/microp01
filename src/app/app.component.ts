22d2665e98595f7c7b0499678919cc0c47eda73a
Pablo Berciano <100363964@alumnos.uc3m.es>
2019-02-04 16:33:33 +0100

    Solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'microp01';
+  title = 'Micro-práctica 1';
+
+  private productos:Array<[string,number]>=[['Arroz tres delicias',3.50],['Cerdo agridulce',5.50],['Ternera con bambu y setas',7.00]];
+    
+  public codProducto=-1;
+  public importe:number=0.0;
+   
+  public lineas:Array<[string,number]>=[];
+  
+  public addLinea()
+  {
+	  var producto=this.productos[this.codProducto][0];
+	  var precio:number=this.productos[this.codProducto][1];
+	  this.lineas.push([producto,precio]);
+	  this.importe=this.importe+precio;
+  }
+  
+  public delLinea(i)
+  {
+	  this.importe-=this.lineas[i][1];
+	  this.lineas.splice(i,1);
+  }
 }
